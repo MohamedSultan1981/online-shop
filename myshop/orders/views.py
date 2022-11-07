0d8144b2994d2d9ef9c74fde5b687763a5ebd357
MohamedSultan1981 <50361347+MohamedSultan1981@users.noreply.github.com>
2022-11-07 17:44:11 +0200

    task

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 # Create your views here.
 def order_create(request):
     cart = Cart(request)
@@ -13,7 +14,9 @@ def order_create(request):
                 OrderItem.objects.create(order=order,product=item['product'],price=item['price'],quantity=item['quantity'])
             # clear the cart
             cart.clear()
-            return render(request,'orders/order/created.html',{'order': order})
+            # launch asynchronous task
+            order_created.delay(order.id)
+        return render(request,'orders/order/created.html',{'order': order})
     else:
         form = OrderCreateForm()
         return render(request,'orders/order/create.html',{'cart': cart, 'form': form})
